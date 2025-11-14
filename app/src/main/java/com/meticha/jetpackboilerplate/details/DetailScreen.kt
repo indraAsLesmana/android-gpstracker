946358462ba29fce2b53735774467eeb8bc5224a
Indra Agus Lesmana <indra953@gmail.com>
2025-11-15 06:51:28 +0700

    implement fused location on DetailScreen.kt

--- app/src/main/java/com/meticha/jetpackboilerplate/details/DetailScreen.kt
+++ app/src/main/java/com/meticha/jetpackboilerplate/details/DetailScreen.kt
@@ -1,20 +1,61 @@
+
 package com.meticha.jetpackboilerplate.details
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun DetailsScreen(viewModel: DetailScreenViewModel = hiltViewModel()) {
+    val location by viewModel.location.collectAsState()
+    val context = LocalContext.current
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            viewModel.startLocationUpdates()
+        } else {
+            // Handle permission denial
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                viewModel.startLocationUpdates()
+            }
+            else -> {
+                launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
     Scaffold {
         Column(
             modifier = Modifier.padding(it)
         ) {
             Text("Details Screen")
+            location?.let {
+                Text("Latitude: ${it.latitude}")
+                Text("Longitude: ${it.longitude}")
+            }
         }
     }
 }
--- app/src/main/java/com/meticha/jetpackboilerplate/details/DetailScreenViewModel.kt
+++ app/src/main/java/com/meticha/jetpackboilerplate/details/DetailScreenViewModel.kt
@@ -1,16 +1,47 @@
+
 package com.meticha.jetpackboilerplate.details
 
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
-class DetailScreenViewModel @Inject constructor() : ViewModel() {
-    init {
-        println("init started of details")
+@HiltViewModel
+class DetailScreenViewModel @Inject constructor(
+    private val fusedLocationProviderClient: FusedLocationProviderClient
+) : ViewModel() {
+
+    private val _location = MutableStateFlow<Location?>(null)
+    val location = _location.asStateFlow()
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            _location.value = locationResult.lastLocation
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates() {
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY, 10000
+        ).build()
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
     }
 
     override fun onCleared() {
-        println("init cleared of details")
         super.onCleared()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
-
 }
