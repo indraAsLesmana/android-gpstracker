30bff49f7374295bb3e60a27571cfffb4eeef3ce
Indra Agus Lesmana <indra953@gmail.com>
2025-11-15 06:44:27 +0700

    add fused location service library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
     implementation(libs.kotlinx.serialization.core)
     implementation(libs.kotlinx.serialization.json)
 
+    implementation(libs.play.services.location)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
