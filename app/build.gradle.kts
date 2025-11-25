db096fb19de79137695fca706dedbf37ffd4dc82
zacky14 <zacky.inside@gmail.com>
2025-11-25 14:06:24 +0700

    fixing necessary lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3.android)
-    implementation(libs.play.services.maps)
 
     implementation("com.google.maps.android:maps-utils-ktx:5.2.1")
 
